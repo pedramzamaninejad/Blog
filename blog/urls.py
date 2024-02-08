47abf973903a7a3d1be76c038e15abaa72796eb1
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-02-08 17:37:05 +0330

    adding blog update functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView, BlogUpdateView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('list/', BlogListView.as_view(), name='blog-list'),
-    path('create/', BlogCreateView.as_view(), name='blog-create')
+    path('create/', BlogCreateView.as_view(), name='blog-create'),
+    path('<int:pk>', BlogUpdateView.as_view(), name='blog-update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Blog
 from .forms import BlogForm
@@ -12,7 +13,7 @@ class BlogListView(generic.ListView):
     context_object_name = 'blogs'
 
 
-class BlogCreateView(generic.CreateView):
+class BlogCreateView(LoginRequiredMixin, generic.CreateView):
     form_class = BlogForm
     template_name = 'blog/blog_create.html'
     success_url = reverse_lazy('blog:blog-list')
@@ -20,3 +21,13 @@ class BlogCreateView(generic.CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Blog
+    fields = ['title', 'blog', 'status']
+    template_name = 'blog/blog_update.html'
+
+    def test_func(self):
+        blog_user = self.get_object().author
+        return blog_user == self.request.user
