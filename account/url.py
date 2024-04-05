1613ce74c9b662d850dc0e30f0209463581a1681
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-04-05 16:20:50 +0330

    add profile route and view

--- account/url.py
+++ account/url.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-from .views import RegisterView, CustomLogoutView
+from .views import RegisterView, ProfileView
 
 app_name = 'account'
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
-    path('signout/', CustomLogoutView.as_view(), name='logout'),
+    path('signout/', TemplateView.as_view(template_name='registration/logout.html'), name='signout'),
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
--- account/views.py
+++ account/views.py
@@ -1,18 +1,24 @@
 from django.views import generic
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .form import CustomUserCreationForm
+from .form import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 class RegisterView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = "registration/register.html"
     success_url = reverse_lazy('login')
 
+class ProfileView(LoginRequiredMixin, generic.TemplateView):
+    model = CustomUser
+    template_name = 'account/profile.html'
+    http_method_names = ['get', 'options']
 
-class CustomLogoutView(LogoutView):
-    http_method_names = ['post', 'get', 'options']
-    template_name = 'registration/logout.html'
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        context['user'] = CustomUser.objects.get(id=self.request.user.id)
 
-    def get_success_url(self):
-        return reverse_lazy('blog:list')
+        print(context['user'].first_name)
+
+        return context
